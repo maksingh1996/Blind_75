d69f1a66ecf32b8c2fdaa064052572c4ca65f681
Manish <manish@Manishs-MacBook-Air.local>
2023-12-23 07:54:52 +0530

    initial commit

---
+++ Maximum_Product_Subarray.py
@@ -0,0 +1,15 @@
+def MaxProdSubArray(arr):
+    res = max(arr)
+    curMin, curMax = 1, 1
+
+    for data in arr:
+        if data == 0:
+            curMin, curMax = 1, 1
+            continue
+        tmp = data*curMax
+        curMax = max(data*curMax, data*curMin, data)
+        curMin = min(data*curMax, data*curMin, data)
+        res = max(res, curMax)
+    print(res)
+
+MaxProdSubArray([5])
